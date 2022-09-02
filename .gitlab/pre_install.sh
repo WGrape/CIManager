97c82237b369c1cd4e6f86399772ed5ee99bc343
lvsi <15239928381@163.com>
2022-09-03 01:12:42 +0800

    feat: 修改pre_install的curl安装

--- .gitlab/pre_install.sh
+++ .gitlab/pre_install.sh
@@ -5,6 +5,8 @@ echo -e "1. Print the variables"
 PrintEnv
 
 # Complete the installation of all dependencies
-echo -e "2. "apt-get update && apt-get install -y curl
+echo -e "2. apt-get update && apt-get install -y curl"
+apt-get update
+apt-get install -y curl
 
 echo -e "------------ The script pre_install.sh is stopped ------------"
