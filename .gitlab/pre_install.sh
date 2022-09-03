3f28a36c24710e0d3448d0335c61919ce37b7414
lvsi <15239928381@163.com>
2022-09-03 14:55:53 +0800

    fix: pre_install修改apt镜像

--- .gitlab/pre_install.sh
+++ .gitlab/pre_install.sh
@@ -6,8 +6,11 @@ PrintEnv
 
 # Complete the installation of all dependencies
 echo -e "2. apt-get update && apt-get install -y curl"
-apt-get update
+mv /etc/apt/sources.list /etc/apt/sources.list.bak \
+    && echo 'deb http://mirrors.163.com/debian/ stretch main non-free contrib' > /etc/apt/sources.list \
+    && echo 'deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib' >> /etc/apt/sources.list \
+    && echo 'deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib' >> /etc/apt/sources.list \
+    && apt-get update
 apt-get install -y curl
-#apt-get install -y curl
 
 echo -e "------------ The script pre_install.sh is stopped ------------"
