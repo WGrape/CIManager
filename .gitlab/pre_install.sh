f477e21ea8b71b34c3531c2fb9572e4142e290e4
lvsi <15239928381@163.com>
2022-09-03 14:40:03 +0800

    fix: 修改pre_install不支持sudo

--- .gitlab/pre_install.sh
+++ .gitlab/pre_install.sh
@@ -6,8 +6,8 @@ PrintEnv
 
 # Complete the installation of all dependencies
 echo -e "2. apt-get update && apt-get install -y curl"
-sudo apt-get update
-sudo apt-get install curl
+apt-get update
+apt-get install -y curl
 #apt-get install -y curl
 
 echo -e "------------ The script pre_install.sh is stopped ------------"
