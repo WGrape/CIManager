92a8f76ee6bc49f7ef24d39eacc235e79444842b
lvsi <15239928381@163.com>
2022-10-15 16:02:31 +0800

    refactor: make the start.sh and pre_check.sh more simple

--- .gitlab/pre_check.sh
+++ .gitlab/pre_check.sh
@@ -6,22 +6,48 @@ PrintEnv
 
 echo -e "2. Check the must variables"
 
-if [ "${LOCAL_BUILD_TRIGGER_CMD}" == "" ] ; then
-  FAILURE_REASON="Failed to run pre_check.sh: the variables LOCAL_BUILD_TRIGGER_CMD empty"
-  SendFailureNotice
-  exit 1
+if [ "${DING_NOTICE_SWITCH}" == "on" ]; then
+  if [ "${DING_ACCESS_TOKEN}" == "" ]; then
+    FAILURE_REASON="Failed to run pre_check.sh: the variables DING_ACCESS_TOKEN empty"
+    SendFailureNotice
+    exit 1
+  fi
 fi
 
-if [ "${HEALTH_CHECK_TRIGGER_CMD}" == "" ] ; then
-  FAILURE_REASON="Failed to run pre_check.sh: the variables HEALTH_CHECK_TRIGGER_CMD empty"
-  SendFailureNotice
-  exit 1
+if [ "${UNIT_TEST_SWITCH}" == "on" ]; then
+  if [ "${UNIT_TEST_TRIGGER_CMD}" == "" ]; then
+    FAILURE_REASON="Failed to run pre_check.sh: the variables UNIT_TEST_TRIGGER_CMD empty"
+    SendFailureNotice
+    exit 1
+  fi
 fi
 
-if [ "${HEALTH_CHECK_SUCCESS}" == "" ] ; then
-  FAILURE_REASON="Failed to run pre_check.sh: the variables HEALTH_CHECK_SUCCESS empty"
-  SendFailureNotice
-  exit 1
+if [ "${APIDOC_SWITCH}" == "on" ]; then
+  if [ "${APIDOC_TRIGGER_CMD}" == "" ] || [ "${APIDOC_FILE}" == "" ] ; then
+    FAILURE_REASON="Failed to run pre_check.sh: the variables APIDOC_TRIGGER_CMD/APIDOC_FILE empty"
+    SendFailureNotice
+    exit 1
+  elif [ "${GITLAB_HOST}" == "" ] || [ "${GITLAB_API_TOKEN}" == "" ] || [ "${PROJECT_ID}" == "" ] ; then
+    FAILURE_REASON="Failed to run pre_check.sh: the variables GITLAB_HOST/GITLAB_API_TOKEN/PROJECT_ID empty"
+    SendFailureNotice
+    exit 1
+  fi
 fi
 
-echo -e "------------ The script pre_install.sh is stopped ------------"
+if [ "${LOCAL_BUILD_SWITCH}" == "on" ]; then
+  if [ "${LOCAL_BUILD_TRIGGER_CMD}" == "" ] ; then
+    FAILURE_REASON="Failed to run pre_check.sh: the variables LOCAL_BUILD_TRIGGER_CMD empty"
+    SendFailureNotice
+    exit 1
+  fi
+fi
+
+if [ "${HEALTH_CHECK_SWITCH}" == "on" ]; then
+  if [ "${HEALTH_CHECK_TRIGGER_CMD}" == "" ] || [ "${HEALTH_CHECK_SUCCESS}" == "" ] ; then
+    FAILURE_REASON="Failed to run pre_check.sh: the variables HEALTH_CHECK_TRIGGER_CMD/HEALTH_CHECK_SUCCESS empty"
+    SendFailureNotice
+    exit 1
+  fi
+fi
+
+echo -e "------------ The script pre_check.sh is stopped ------------"
--- start.sh
+++ start.sh
@@ -12,39 +12,59 @@ CIManager Running Result: pre_install.sh failed"
   exit 1
 fi
 
-bash ./.gitlab/check_code.sh
-if [ $? -ne 0 ]; then
-  echo -e "
-CIManager Running Result: check_code.sh failed"
-  exit 1
+if [ "${CHECK_CODE_SWITCH}" == "on" ]; then
+  bash ./.gitlab/check_code.sh
+  if [ $? -ne 0 ]; then
+    echo -e "
+  CIManager Running Result: check_code.sh failed"
+    exit 1
+  fi
+else
+  echo "apidoc_gen.sh is skipped due to the switch is off"
 fi
 
-bash ./.gitlab/unit_test.sh
-if [ $? -ne 0 ]; then
-  echo -e "
-CIManager Running Result: unit_test.sh failed"
-  exit 1
+if [ "${UNIT_TEST_SWITCH}" == "on" ]; then
+  bash ./.gitlab/unit_test.sh
+  if [ $? -ne 0 ]; then
+    echo -e "
+  CIManager Running Result: unit_test.sh failed"
+    exit 1
+  fi
+else
+  echo "unit_test.sh is skipped due to the switch is off"
 fi
 
-bash ./.gitlab/apidoc_gen.sh
-if [ $? -ne 0 ]; then
-  echo -e "
-CIManager Running Result: apidoc_gen.sh failed"
-  exit 1
+if [ "${APIDOC_SWITCH}" == "on" ]; then
+  bash ./.gitlab/apidoc_gen.sh
+  if [ $? -ne 0 ]; then
+    echo -e "
+  CIManager Running Result: apidoc_gen.sh failed"
+    exit 1
+  fi
+else
+  echo "apidoc_gen.sh is skipped due to the switch is off"
 fi
 
-bash ./.gitlab/local_build.sh
-if [ $? -ne 0 ]; then
-  echo -e "
-CIManager Running Result: local_build.sh failed"
-  exit 1
+if [ "${LOCAL_BUILD_SWITCH}" == "on" ]; then
+  bash ./.gitlab/local_build.sh
+  if [ $? -ne 0 ]; then
+    echo -e "
+  CIManager Running Result: local_build.sh failed"
+    exit 1
+  fi
+else
+  echo "local_build.sh is skipped due to the switch is off"
 fi
 
-bash ./.gitlab/health_check.sh
-if [ $? -ne 0 ]; then
-  echo -e "
-CIManager Running Result: health_check.sh failed"
-  exit 1
+if [ "${HEALTH_CHECK_SWITCH}" == "on" ]; then
+  bash ./.gitlab/health_check.sh
+  if [ $? -ne 0 ]; then
+    echo -e "
+  CIManager Running Result: health_check.sh failed"
+    exit 1
+  fi
+else
+  echo "health_check.sh is skipped due to the switch is off"
 fi
 
 echo -e "
