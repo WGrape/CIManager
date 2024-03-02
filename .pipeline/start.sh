2e2973c8b9400c2972b15eadcbbf17092b8f41f5
lvsi <15239928381@163.com>
2024-03-02 23:57:25 +0800

    fix: fix the yaml error

--- .pipeline/start.sh
+++ .pipeline/start.sh
@@ -3,27 +3,28 @@
 CUR_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
 cd $CUR_PATH && cd ../
 BATH_PATH=$(pwd)
-. $BATH_PATH/internal/helper.sh
+. $BATH_PATH/.pipeline/internal/helper.sh
 
 job_queue = (
     # ========================= CI =========================
     # [stage] = pre
-    "ci.stage_pre.job_pre_check"
-    "ci.stage_pre.job_pre_install"
+    "ci.stage_pre.job_pre_check" \
+    "ci.stage_pre.job_pre_install" \
 
     # [stage] = test
-    "ci.stage_test.job_check_code"
-    "ci.stage_test.job_unit_test"
+    "ci.stage_test.job_check_code" \
+    "ci.stage_test.job_unit_test" \
 
     # [stage] = build
-    "ci.stage_build.job_apidoc_gen"
-    "ci.stage_build.job_local_build"
+    "ci.stage_build.job_apidoc_gen" \
+    "ci.stage_build.job_local_build" \
     # ========================= CD =========================
     # [stage] = deploy
-    "cd.stage_deploy.job_ssh"  # "cd.stage_deploy.job_pull_artifact"
+    # "cd.stage_deploy.job_pull_artifact"
+    "cd.stage_deploy.job_ssh" \
     # [stage] = monitor
-    "cd.stage_monitor.job_api_test"
-    "cd.stage_monitor.job_health_check"
+    "cd.stage_monitor.job_api_test" \
+    "cd.stage_monitor.job_health_check" \
 )
 
 for job_name in "${job_queue[@]}"; do
