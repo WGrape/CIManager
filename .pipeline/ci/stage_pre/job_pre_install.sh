d3470e4f0e6356e3f945608a921bf9a6d779127f
lvsi <15239928381@163.com>
2024-03-03 12:32:14 +0800

    update: update pipeline.yml

--- .pipeline/ci/stage_pre/job_pre_install.sh
+++ .pipeline/ci/stage_pre/job_pre_install.sh
@@ -3,9 +3,9 @@
 # complete the installation of all dependencies
 echo "1. apt-get update && apt-get install -y curl"
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak \
-    && echo 'deb http://mirrors.163.com/debian/ stretch main non-free contrib' > /etc/apt/sources.list \
-    && echo 'deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib' >> /etc/apt/sources.list \
-    && echo 'deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib' >> /etc/apt/sources.list \
+    && sudo echo 'deb http://mirrors.163.com/debian/ stretch main non-free contrib' > /etc/apt/sources.list \
+    && sudo echo 'deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib' >> /etc/apt/sources.list \
+    && sudo echo 'deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib' >> /etc/apt/sources.list \
     && apt-get update -y
 apt-get install curl -y
 
