f74b9dedfcc144e0fab7fdc8a585d5c5a7df0cb3
lvsi <15239928381@163.com>
2024-03-03 12:35:03 +0800

    update: update pipeline.yml

--- .pipeline/ci/stage_pre/job_pre_install.sh
+++ .pipeline/ci/stage_pre/job_pre_install.sh
@@ -6,7 +6,7 @@ sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak \
     && sudo echo 'deb http://mirrors.163.com/debian/ stretch main non-free contrib' > /etc/apt/sources.list \
     && sudo echo 'deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib' >> /etc/apt/sources.list \
     && sudo echo 'deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib' >> /etc/apt/sources.list \
-    && apt-get update -y
+    && sudo apt-get update -y
 apt-get install curl -y
 
 # if trigger cmd exists, then execute it.
