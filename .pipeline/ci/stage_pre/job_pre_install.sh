a648b9a81e61339c889480441d39a18507286439
lvsi <15239928381@163.com>
2024-03-04 10:14:51 +0800

    feat: add install docker and docker-compose in job_pre_install.sh

--- .pipeline/ci/stage_pre/job_pre_install.sh
+++ .pipeline/ci/stage_pre/job_pre_install.sh
@@ -11,6 +11,11 @@ mv /etc/apt/sources.list /etc/apt/sources.list.bak \
 echo "2. install curl"
 apt-get install curl -y
 
+echo "2. install docker and docker-compose"
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # if trigger cmd exists, then execute it.
 if [ "${global_job_cmd}" != "" ]; then
     if ! eval "${global_job_cmd}"; then
